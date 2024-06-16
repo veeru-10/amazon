d29fbc8dc78e4c9e961e1fdf4ac2de0e449b4500
Toxic <Toxic10@.dev>
2024-06-16 07:31:02 +0530

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import {renderOrderSummery} from "./checkouts/orderSummery.js";
 import {renderPaymentSummary} from './checkouts/paymentSummery.js';
+import '../data/cart-oop.js';
 renderOrderSummery();
 renderPaymentSummary();
