33d1f03f25de6b99ba5bf75d2443425ac8877c3d
Toxic <Toxic10@.dev>
2024-06-16 08:04:48 +0530

    generate cart using class oop

---
+++ data/cart-class.js
@@ -0,0 +1,91 @@
+//class is a better way to generate objects in oop
+
+class Cart {   //use pascal case -Class- for oop, it is a object generator
+ cartItems;//cartItems = undefined;
+ localStorageKey;//localStorageKey = undefined;
+
+ constructor(localStorageKey) {
+  this.localStorageKey = localStorageKey;
+  this.loadFromStorage();
+ }
+
+ loadFromStorage() {
+  cart.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+ 
+   if (!this.cartItems) {
+   this.cartItems = [{
+     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+     quantity: 2,
+     deliveryOptionId: '1'
+   }, {
+     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+     quantity: 1,
+     deliveryOptionId: '2'
+   }]; 
+   }
+ }
+
+ saveToStorage() {
+  localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+}
+
+
+addToCart(productId) {
+ let matchingItem;
+ this.cartItems.forEach((cartItem) => {
+  if (productId === cartItem.productId) {
+    matchingItem = cartItem;
+  }
+ });
+ 
+ if (matchingItem) {
+   matchingItem.quantity += 1;
+ } else {
+   this.cartItems.push({
+     productId,
+     quantity: 1,
+     deliveryOptionId: '1'
+   });
+}
+ this.saveToStorage();
+}
+
+removeFromCart(productId) {
+ const newCart = [];
+ this.cartItems.forEach((cartItem) => {
+  if (cartItem.productId != productId) {
+   newCart.push(cartItem);
+  }
+ });
+ 
+ this.cartItems = newCart;
+ this.saveToStorage();
+ }
+
+
+ updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  this.cartItems.forEach((cartItem) => {
+   if (productId === cartItem.productId) {
+     matchingItem = cartItem;
+   }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  this.saveToStorage();
+}
+
+}
+
+
+const cart = new Cart('cart-oop');//position of curly brackets used for constrctors
+
+const businessCart = new Cart('business-oop');
+
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);//true
+
+
+//instance = an object generated from a class is known as instance example Cart, businessCart; 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import {renderOrderSummery} from "./checkouts/orderSummery.js";
 import {renderPaymentSummary} from './checkouts/paymentSummery.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 renderOrderSummery();
 renderPaymentSummary();
